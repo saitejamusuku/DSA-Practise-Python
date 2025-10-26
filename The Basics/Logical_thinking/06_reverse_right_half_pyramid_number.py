7048fb5d0d5ba75945bebf3a9b6007e54424f103
22891a0533saiteja <22891a0533saiteja@gmail.com>
2025-10-26 20:32:51 +0530

    6 Patterns done

---
+++ The Basics/Logical_thinking/06_reverse_right_half_pyramid_number.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number:"))
+
+for i in range(0,n+1):
+    for j in range(1,n-i+1,1):
+        print(j, end = " ")
+    print()
+
+# 1 2 3 4 5 
+# 1 2 3 4 
+# 1 2 3 
+# 1 2 
+# 1 
+
+
